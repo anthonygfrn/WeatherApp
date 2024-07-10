d2d49606f36da813ae7de617bce2d8d4391b4217
Anthony Gufron <81062477+anthonygfrn@users.noreply.github.com>
2024-07-10 17:12:44 +0700

    Update using timer

--- WeatherApp/Models/LocationManager.swift
+++ WeatherApp/Models/LocationManager.swift
@@ -12,6 +12,7 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate{
     
     var locationManager = CLLocationManager()
     @Published var authorisationStatus: CLAuthorizationStatus?
+    @Published var cityName: String = "Unknown Location"
     
     var latitude: Double{
         locationManager.location?.coordinate.latitude ?? 37.32298
@@ -23,6 +24,7 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate{
     override init(){
         super.init()
         locationManager.delegate = self
+        fetchCityName()
     }
     
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
@@ -52,8 +54,21 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate{
         }
     }
     
+    func fetchCityName() {
+            guard let location = locationManager.location else { return }
+            let geocoder = CLGeocoder()
+            geocoder.reverseGeocodeLocation(location) { [weak self] (placemarks, error) in
+                guard let self = self else { return }
+                if let placemark = placemarks?.first, let city = placemark.locality {
+                    DispatchQueue.main.async {
+                        self.cityName = city
+                    }
+                }
+            }
+        }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
+        fetchCityName()
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
--- WeatherApp/Views/ContentView.swift
+++ WeatherApp/Views/ContentView.swift
@@ -7,26 +7,32 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct ContentView: View {
     @ObservedObject var weatherKitManager = WeatherKitManager()
-    
     @StateObject var locationManager = LocationManager()
+    let timer = Timer.publish(every: 60, on: .main, in: .common).autoconnect() // Update every 60 seconds
     
     var body: some View {
-        
         if locationManager.authorisationStatus == .authorizedWhenInUse {
-            
             // create your view
-            VStack{
-                Label(weatherKitManager.temp, systemImage: weatherKitManager.symbol)
+            VStack {
+                Text("You're in \(locationManager.cityName)")
+                    .font(.largeTitle)
+                    .padding(.bottom, 10)
                 
-                Text(locationManager)
+                Label(weatherKitManager.temp, systemImage: weatherKitManager.symbol)
             }
-                .task {
+            .task {
+                await weatherKitManager.getWeather(latitude: locationManager.latitude, longitude: locationManager.longitude)
+            }
+            .onReceive(timer) { _ in
+                Task {
                     await weatherKitManager.getWeather(latitude: locationManager.latitude, longitude: locationManager.longitude)
                 }
+            }
         } else {
-            
             // Create your alternate view
             Text("Error loading location")
         }
